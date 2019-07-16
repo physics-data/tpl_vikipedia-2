879db8953e50ee964c75301135866fe537d13fa6
Jiajie Chen <noc@jiegec.ac.cn>
2019-07-16 16:10:09 +0800

    Note -i.bak in replace.sh and README

--- replace.sh
+++ replace.sh
@@ -9,10 +9,14 @@ laundary_data() {
     for i in ""; do # how to get the list of files & subdirectories?
         if ???; then # check if $i is directory
             # $i is directory, recursively call function
+            echo 'FIXME'
         else
             # $i is file, do replace
             for rule in ${RULES}; do
                 # apply rule like 'foo/bar' with sed
+                # use 'sed -i.bak' to replace file inplace
+                # the grader will skip .bak files for comparison
+                echo 'FIXME'
             done
         fi
     done
